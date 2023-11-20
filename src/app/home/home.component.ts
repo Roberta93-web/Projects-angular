353640f6f5ddaac85a297fd0162bd68330067812
Roberta <federicoroberta1@gmail.com>
2023-11-20 17:33:39 +0100

    add navbar responsive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  menuVariable: boolean = false;
+  menu_icon_variable: boolean = false;
+  openMenu() {
+    this.menuVariable = !this.menuVariable;
+    this.menu_icon_variable = !this.menu_icon_variable;
+  }
 
+  images = [{ img1: '' }, { img1: '' }, { img1: '' }];
 }
